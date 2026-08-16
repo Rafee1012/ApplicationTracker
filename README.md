667dd9903c9fd0fa98107f117cba7cd4106544cc
Rafee1012 <rafee.ahsan5@gmail.com>
2026-08-15 17:41:56 -0700

    Implemented ASP.NET Core backend server

--- README.md
+++ README.md
@@ -2,10 +2,14 @@
 For Tracking
 ---
 
-Run all commands with ```cd src/ApplicationTracker```.
+Run domain commands with ```cd src/ApplicationTracker```.
 - Build project with: ```dotnet build```.
 - Run project with: ```dotnet run```.
 - Check packages with: ```dotnet list package```.
+
+Run API commands with ```cd src/ApplicationTracker.Api```.
+- Build project with: ```dotnet build```.
+- Run project with: ```dotnet run```.
 
 Run tests commands with ```cd tests/ApplicationTracker.tests```.
 - Run tests with: ```dotnet test```.
---
+++ src/ApplicationTracker.Api/Controllers/ApplicationController.cs
@@ -0,0 +1,78 @@
+using Microsoft.AspNetCore.Mvc;
+using ApplicationTracker.Domain;
+
+public class UpdateApplicationRequest
+{
+    public ApplicationStatus Status { get; set; }
+}
+
+[ApiController]
+[Route("api/applications")]
+public class ApplicationController : ControllerBase
+{
+    ApplicationFolder folder = new ApplicationFolder();
+
+    [HttpGet]
+    public IActionResult GetApplications()
+    {
+        return Ok(folder.Applications);
+    }
+
+    [HttpGet("{id:guid}")]
+    public IActionResult GetApplication(Guid id)
+    {
+        try
+        {
+            Application app = folder.GetApplicationByID(id);
+            return Ok(app);
+        }
+        catch
+        {
+            return NotFound("No application with that ID");
+        }
+    }
+
+    [HttpPost]
+    public IActionResult AddApplication(Application application)
+    {
+        try
+        {
+            folder.AddApplication(application);
+            return Ok(application);
+        }
+        catch
+        {
+            return Conflict("Application already exists");
+        }
+    }
+
+    [HttpDelete("{id:guid}")]
+    public IActionResult DeleteApplication(Guid id)
+    {
+        try
+        {
+            Application app = folder.GetApplicationByID(id);
+            folder.RemoveApplication(app);
+            return Ok(app);
+        }
+        catch
+        {
+            return NotFound("No application with that ID");
+        }
+    }
+
+    [HttpPut("{id:guid}")]
+    public IActionResult UpdateApplication(Guid id, UpdateApplicationRequest request)
+    {
+        try
+        {
+            folder.UpdateApplicationStatus(id, request.Status);
+            Application app = folder.GetApplicationByID(id);
+            return Ok(app);
+        }
+        catch
+        {
+            return NotFound("No application with that ID");
+        }
+    }
+}
